bba9c8ceb89c5c41b3158a87ce49e3bad483f219
Alissa Schlecht <alissa@hanen.local>
2019-09-12 18:36:21 -0400

    add search component

---
+++ countries/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './SearchBar.scss';
+
+
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: ''
+		}
+	}
+
+	search() {
+		console.log('search', this.state.query);
+	}
+
+	render(){
+		return (
+			<div>
+				<label>Search</label>
+				<input 
+					type="text"
+					placeholder="search for a country"
+					onChange={event => this.setState({query: event.target.value})}
+					onKeyPress={event => {
+							if (event.key === 'Enter') {
+								this.search();
+							}
+						}
+					}
+				/>
+				<button onClick={() => this.search()}>
+					Search
+				</button>
+			</div>
+		)
+	}
+}
+
+// const SearchBar = () => {
+// 	return (
+		
+// 	)
+// }
+
+export default SearchBar;
